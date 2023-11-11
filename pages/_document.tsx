c8dcd697ed5f61e19ff3f429d948c033a17aa847
mattius459 <matt.ahlborg@gmail.com>
2023-11-10 20:29:13 -0500

    fixed umami again

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,25 @@
 import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
 
+import i18nextConfig from '../next-i18next.config';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <script async src="https://analytics.eu.umami.is/script.js" data-website-id="587e1a93-6ec8-4d38-a075-43faed565ab1"></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+type Props = DocumentProps & {
+  // add custom document props
+};
+
+export default function Document(props: Props) {
+  const currentLocale =
+    props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+  return (
+    <Html lang={currentLocale}>
+      <Head>
+        <script async src="https://analytics.eu.umami.is/script.js" data-website-id="587e1a93-6ec8-4d38-a075-43faed565ab1"></script>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
