d6fa758770ccbfb70a8672770732d68c5937c166
mattius459 <matt.ahlborg@gmail.com>
2023-11-12 19:07:00 -0600

    changed name to okCompute

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -432,7 +432,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'okCompute'
                     )}
                   </div>
 
