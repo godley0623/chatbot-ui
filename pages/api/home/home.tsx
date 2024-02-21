537e9c261a6bda6eed32168a28638755823a2919
mattius459 <matt.ahlborg@gmail.com>
2024-02-21 17:42:37 -0500

    added kickass flame favicon

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -390,7 +390,7 @@ const Home = ({
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="flame.png" />
       </Head>
       {selectedConversation && (
         <main
