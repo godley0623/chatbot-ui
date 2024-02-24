32dd7cb6d54f4e6ff9aadd3aa236313117bcf19f
mattius459 <matt.ahlborg@gmail.com>
2024-02-24 12:43:24 -0500

    asdf

--- components/Payments/accountIDModal.js
+++ components/Payments/accountIDModal.js
@@ -2,30 +2,24 @@ import Swal from 'sweetalert2';
 import { getCreditIdBalance } from './getCreditIdBalance';
 
 const accountIDModal = (accountId, fullCreditId) => {
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  return async () => { // Make this function async
+  return async () => { // The function is now async
     try {
       // Fetch the account data from your backend
-      const response = await getCreditIdBalance(fullCreditId);
-      console.log('accountIDModal response:', response);
-      if (response.ok) {
-        const accountData = await response.json();
-        console.log('accountData:', accountData);
-        // Now use the retrieved data in your modal
-        Swal.fire({
-          title: 'Account ID',
-          html: `<p>Your Account ID is: ${accountId}</p>
-                 <p>Balance (satoshis): ${accountData.creditRecord.amount}</p>`,
-        });
-      } else {
-        // Handle errors or cases where the account is not found
-        Swal.fire({
-          title: 'Error',
-          text: 'Failed to retrieve account data',
-          icon: 'error',
-        });
+      const accountData = await getCreditIdBalance(fullCreditId);
+      console.log('accountIDModal response:', accountData);
+
+      if (accountData.error) {
+        throw new Error(accountData.error);
       }
+
+      console.log('accountData:', accountData.creditRecord);
+      // Now use the retrieved data in your modal
+      Swal.fire({
+        title: 'Account ID',
+        html: `<p>Your Account ID is: ${accountId}</p>
+               <p>Balance (satoshis): ${accountData.creditRecord.amount}</p>`,
+      });
+
     } catch (error) {
       console.error('Failed to fetch account data:', error);
       Swal.fire({
